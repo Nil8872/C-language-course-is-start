f05a3f4672b88cc21615ed5b8f181d6b6df242e5
Nilesh Sariya <sariyanilesh7265@gmail.com>
2022-07-30 23:39:26 +0530

    User input is added into Reverse Array

--- chapter 6/ReverseArray.c
+++ chapter 6/ReverseArray.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
 
 int main()
-{//   int n;
-    int arr[5];
+{
+    int n;
+    printf("Enter the number of how many element :");
+    scanf("%d", &n);
+    int arr[n];
 
     int *ptr = arr;
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < (n); i++)
     {
-        printf("Enter %d element of arrays : ", i + 1);
+        printf("Enter %d element : ", i + 1);
         scanf("%d", &arr[i]);
         ptr++;
     }
+    int *ptr1 = arr;
+    printf("Your Array is : ");
+
+    printf("{");
+    for (int i = 0; i <n; i++){
+        if(i==(n-1)){
+        printf("%d",*ptr1);
+        ptr1++;
+
+        }
+        else{
+
+        printf("%d, ",*ptr1);
+        ptr1++;
+        }
+    }
+    printf("}\n");
+
     printf("Your Reverse Array is : ");
     printf("{");
-    for (int i = 5; i > 0; i--)
+    for (int i = n; i > 0; i--)
     {
-        if (i==1)
+        if (i == 1)
         {
             ptr--;
             printf("%d", *ptr);
