ad80b4bd7de5e60260fa9f0d96dd589bde232adb
Nilesh Sariya <sariyanilesh7265@gmail.com>
2022-08-03 13:48:16 +0530

    String are compare %s and %c getting input

--- String/inputStringUsing%c.c
+++ String/inputStringUsing%c.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
@@ -16,7 +17,12 @@ int main()
         i++;
         // printf("%c", ch);
     }
-    st[i] = '\0';
+    st[i-1] = '\0';
     printf("%s", st);
+
+    char st1[] = "Nilesh";
+    printf("%s\n", st1);
+
+    printf("value of cmp is %d", strcmp(st1,st));
     return 0;
 }
