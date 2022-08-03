dd87f163c9ccc7b8a8ca932f5bb51aa6e5abbb61
Nilesh Sariya <sariyanilesh7265@gmail.com>
2022-08-03 23:16:42 +0530

    Typedef is added

--- Structure_In_c/PointerInStucture.c
+++ Structure_In_c/PointerInStucture.c
@@ -1,25 +1,33 @@
 # include<stdio.h>
 
-struct PointerInStucture
+typedef struct PointerInStucture
 {
     int id;
     float salary;
     char name[30];
-};
+} p;
 
 
 int main(){
 
-    struct PointerInStucture p1;
-    struct PointerInStucture *ptr;
+    p p1;
+    p p2;
+    p *ptr2;
+    ptr2 = &p2;
+
+    p *ptr;
     ptr = &p1;
 
     // (*ptr).id = 100;
+    ptr2->id = 200;
     ptr->id = 100;
     ptr->salary = 55.23;
+    ptr2->salary = 99.99;
 
     printf("Employee id is : %d\n", ptr->id);
     printf("Employee salary is  : %f\n", ptr->salary);
+    printf("Employee id is : %d\n", ptr2->id);
+    printf("Employee salary is  : %f\n", ptr2->salary);
      
     
     
